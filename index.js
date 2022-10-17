54df9f4cae8b90085888ca8105ea7beab63fbc59
qwerty9927 <tanvo9927@gmail.com>
2022-10-17 14:31:33 +0700

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ global.__basedir = __dirname
 
 app.use(helmet())
 app.use(cors({
-  origin: [process.env.HOSTACCESS_1, process.env.HOSTACCESS_2]
+  origin: [process.env.HOSTACCESS_1, process.env.HOSTACCESS_2, "http://localhost:3000"]
 }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
