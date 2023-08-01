d0b5916c1c688cfbeea4ee524375f2e9374d8083
Qwerty9927 <tanvo9927@gmail.com>
2023-08-01 20:25:26 +0700

    fix connection

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,19 +3,14 @@ const config = require("../configs")
 
 class Connection {
   static connection = null
-  static connect() {
+  static async connect() {
     const uri = config.DB.URLDB
-    Connection.connection = mongoose.connect(uri).catch((error) => {
+    await mongoose.connect(uri).catch((error) => {
       console.log("Connect failed")
     })
     console.log("Connect to DB success")
   }
-  static getInstance(){
-    if(mongoose.connection.readyState === 0){
-      Connection.connection = Connection.connect()
-    }
-    return Connection.connection
-  }
+
 }
 
 module.exports = Connection
