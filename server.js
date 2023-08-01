dc7fc8e51cc20247f42e4e9c6a63e79c51290101
Qwerty9927 <tanvo9927@gmail.com>
2023-08-01 19:53:01 +0700

    change port

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require("./src/app")
 const config = require("./src/configs")
 
 const PORT = config.APP.PORT
-app.listen(config.APP.PORT, () => {
+app.listen(config.APP.PORT || 3000, () => {
   console.log("Start server on PORT", PORT)
 })
